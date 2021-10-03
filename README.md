de38cae12ed9bbe6811ca3d3678fd32ad866e977
Vinicius Coutinho <vini.coutinho.if@gmail.com>
2021-10-02 22:07:28 -0300

    v01

---
+++ README.md
@@ -0,0 +1,19 @@
+# Buscador de emojis
+
+### Encontre seu emoji aqui 😄
+
+Projeto de estudo. 
+
+Consumindo um conjunto de dados, realizando seu tratamento, criação da lista e pesquisa desses dados.
+
+Feito usando **HTML**, **CSS** e **Javascript puro.** 
+
+Página desktop
+
+![chrome-capture.gif](Untitled%20f05d72baa74845faafbae998384de787/chrome-capture.gif)
+
+Página mobile
+
+![Captura da Web_2-10-2021_21492_127.0.0.1.jpeg](Untitled%20f05d72baa74845faafbae998384de787/Captura_da_Web_2-10-2021_21492_127.0.0.1.jpeg)
+
+PS: Para roda-lo utilize a extensão Live Server
---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+import { arrayEmoji } from "./emojiData.js";
+
+let emojiList = ""
+
+arrayEmoji.forEach((item) => {
+    emojiList += '<li>' + item.symbol + ' ' + item.title + '<p>Clique para copiar</p>'+ '</li>'
+})
+list.innerHTML = emojiList
+
+
+txtSearch.onkeyup = function (e) {
+    emojiList = this.value
+
+    let key = new RegExp(emojiList, "g")
+
+    for (let i in arrayEmoji) {
+        if (arrayEmoji[i].title.match(key) || arrayEmoji[i].keywords.match(key))
+            list.children[i].removeAttribute('style')
+        else
+            list.children[i].style.display = "none"
+    }
+}
+
